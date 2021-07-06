3d690f28f06fbf00c6645656d0334eba912e4aa6
brktrkmn <brkturkmen@gmail.com>
2021-07-06 13:40:23 -0400

    adding dd, fixing files

--- js/ddapp.js
+++ js/ddapp.js
@@ -6,27 +6,32 @@
 //(() => {
 //identify the nodes of interest in the DOM
 const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
-			dropcontainer = document.querySelector(".puzzle-board"),
-			dragimages = document.querySelectorAll(".puzzle-image"),
-			dropzones = document.querySelectorAll(".drop-zone");
+			dropContainer = document.querySelector(".puzzle-board"),
+			dragImages = document.querySelectorAll(".puzzle-image"),
+			dropZones = document.querySelectorAll(".drop-zone");
 
 	// functions go in the middle
 	function swapImages() {
 		// swap out the draggable thumbnail images
-		// update the background image of the drop zone dropcontainer
+		// update the background image of the drop zone dropContainer
 		// 1. get the imageref attribute from the element we're clicking on
 		let imageIndex = this.dataset.imageref,
 				//newImagePath = "images/dd/backGround" + imageIndex,
 				newImagePath = `url(images/dd/backGround${imageIndex}.jpg)`;
 
-		//set the background image of the dropcontainer
-		dropcontainer.style.backgroundImage = newImagePath;
+		//set the background image of the dropContainer
+		dropContainer.style.backgroundImage = newImagePath;
 
 		//debugger;
 
 }
-	function startDrag() {
-		console.log('dragging ' + this.dataset.piecenum);
+	function startDrag(event) {
+		console.log('dragging ' + this.id);
+
+		// save a reference to the element the user is dragging
+		// so that we can retrieve the element later and put it in a drop zone
+		event.dataTransfer.setData("dragTarget", this.id); //MDN drag and drop reference
+		//debugger;
 	}
 
 	function draggedOver(event) {
@@ -35,19 +40,28 @@ const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
 	}
 
 	function dropped(event) {
+		// allow the drop to happen
 		event.preventDefault();
-		console.log('dropped on the element');
-		console.log(event.target.id);
+
+		// if we've already dropped abd appended into a drop zone, then it shouldn't happen
+		// the return statement is a code killer - nothing will execute past this line
+		if (this.children.lenght > 0) { return; }
+
+		// get the reference to the dragged image - saved in the drag function using setData
+		let targetImage = document.querySelector(`#${event.dataTransfer.getData("dragTarget")}`);
+
+		// add it to the zone we dropped the image on
+		this.appendChild(targetImage);
+
+		debugger;
 	}
 
 
 	// event handling at the bottom
-dragimages.forEach(piece => {
-	piece.addEventListener('dragstart', startDrag, false);
-});
+dragImages.forEach(piece => piece.addEventListener('dragstart', startDrag));
 
 
-dropzones.forEach(zone => {
+dropZones.forEach(zone => {
 	zone.addEventListener('drop',dropped, false);
 	zone.addEventListener('dragover',draggedOver, false);
 });
